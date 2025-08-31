21c6d2ecafd0a60e761ea7e88366720712342cbc
Abdalrhman Alhowri <hoabdalrhman@gmail.com>
2025-09-01 00:53:19 +0300

    Update settings.py

--- project/project/settings.py
+++ project/project/settings.py
@@ -54,7 +54,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
 
 
 ROOT_URLCONF = 'project.urls'
